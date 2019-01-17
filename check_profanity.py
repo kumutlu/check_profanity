7a9562ce9d09d8709d272aab8c67adf28d0a20b8
kumutlu <kumutlu@gmail.com>
2019-01-17 22:27:52 +0300

    Add files via upload

--- check_profanity.py
+++ check_profanity.py
@@ -1,18 +1,45 @@
+
+"""
 import urllib.request, urllib.error
+import urllib.parse
 
 
 
 def read_text():
-    quotes = open("C:\movie_quotes.txt")
+    quotes = open(r"C:\movie_quotes.txt")
     contents_of_file = quotes.read()
     print (contents_of_file)
     quotes.close()
     check_profanity(contents_of_file)
 
 def check_profanity(text_to_check):
-    connection = urllib.request.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
-    output = connection.read()
-    print (output)
+
+    connection = urllib.request.urlopen("http://wdylike.appspot.com/?q=" + text_to_check )
+    print(connection.read())
     connection.close()
+
+
+read_text()
+"""
+
+from urllib.request import FancyURLopener
+
+class MyOpener(FancyURLopener):
+    version = "My new User-Agent"
+
+
+def read_text():
+    quotes = open(r"C:\movie_quotes.txt")
+    contents_of_file = quotes.read()
+    #print(contents_of_file)
+    quotes.close()
+    check_profanity(contents_of_file)
+
+
+def check_profanity(text_to_check):
+    myopener = MyOpener()
+    page = myopener.open("http://www.wdylike.appspot.com/?q="+text_to_check)
+    output = page.read()
+    print(output)
 
 read_text()
